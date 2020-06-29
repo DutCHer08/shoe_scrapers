7db09702fbc5b895f85bd14865aba6a92ef75427
ryanborum <37112354+ryanborum@users.noreply.github.com>
2020-06-28 22:03:07 -0400

    Refactored combined scraper + support for UPDATE
    Scraper now attempts to use an UPDATE query with imgFilePath as the row identifier in cases where a duplicate already exists
    Refactored MySQL query building into a utility function
    General refactoring for readability and brevity

--- CombinedScraper.py
+++ CombinedScraper.py
@@ -7,9 +7,13 @@ from bs4 import BeautifulSoup
 import mysql.connector
 from mysql.connector import Error
 
-def scrape(targetUrls):
+DB_HOST = "localhost"
+DB_DATABASE = "drop_shop"
+DB_USER = "drop-shop-acc"
+DB_PASSWORD = "password123"
+
+def scrape_product_properties(targetUrls):
     # KIX Data
-    # Setting up web request
     req = requests.get(targetUrls["kixify_url"],
         headers={"User-Agent": "Mozilla/5.0"},
         verify=False)
@@ -17,14 +21,12 @@ def scrape(targetUrls):
     page_content = req.text
     soup = BeautifulSoup(page_content, 'html.parser')
     rawData = soup.find_all('script', {'type': 'application/ld+json'})
-    # Converting to String data
-    stringData = str(rawData[1])
-    # Removing the <script> tags from response
-    regexedData = re.split("[<>]", stringData)
-    # Loading data into json object
-    jsonData = json.loads(regexedData[2])
 
-    # Clean variables for DB
+    stringData = str(rawData[1]) # Converting to String data
+    regexedData = re.split("[<>]", stringData) # Removing the <script> tags from response
+    jsonData = json.loads(regexedData[2]) # Loading desired data into json object
+
+    # Init. dictionary that represents product
     shoe_dict: Dict[str, Any] = {
         "collabName": targetUrls["collab_name"],
         "brandName": (jsonData['brand']['name']),
@@ -43,21 +45,11 @@ def scrape(targetUrls):
     soup = BeautifulSoup(page_content, 'html.parser')
     rawData = soup.find_all('script', {'type':'application/ld+json'})
 
-    # Converting to String data
     stringData = str(rawData[0])
-
-    # Removing the <script> tags from response
     regexedData = re.split("[<>]", stringData)
-
-    # Loading data into json object
     jsonData = json.loads(regexedData[2])
 
-    # Dirty variables
-    dirtyReleaseDate = (jsonData['releaseDate'])
-    regexedReleaseDate = re.split("(T)", dirtyReleaseDate)
-
-    brandName = (jsonData['brand'])
-
+    regexedReleaseDate = re.split("(T)", (jsonData['releaseDate']))
 
     # Add variables to obj
     shoe_dict["lowestGoatPrice"] = (jsonData['offers']['lowPrice'])
@@ -73,12 +65,8 @@ def scrape(targetUrls):
     soup = BeautifulSoup(page_content, 'html.parser')
     rawData = soup.find_all('script', {'type':'application/ld+json'})
 
-    # Converting to String data
     stringData = str(rawData[1])
-
-    # Removing the <script> tags from response
     regexedData = re.split("[<>]", stringData)
-    # Loading data into json object
     jsonData = json.loads(regexedData[2])
 
     # Add variables to obj
@@ -87,43 +75,67 @@ def scrape(targetUrls):
 
     return shoe_dict
 
-# Starting connection to mysql DB
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='drop_shop',
-                                         user='drop-shop-acc',
-                                         password='password123',
-                                         auth_plugin='mysql_native_password')
-    sql_select_Query = "SELECT * FROM scraper_input"
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute(sql_select_Query)
-    records = cursor.fetchall()
-
-    for row in records:
-        shoe_props = scrape(row)
-        # print(shoe_props)
-
+def get_mysql_query(action, shoe_props):
+    if (action == "insert"):
         SQL_Insert_Query = "INSERT INTO products (brandName, productName, modelNum, productDescription, releaseDate, imgFilePath, collabName, lowestGoatPrice, lowestFcPrice, lowestKixPrice)"
         SQL_Insert_Query += "VALUES (\"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", {}, {}, {} )".format(
-                shoe_props["brandName"],
-                shoe_props["productName"],
-                shoe_props["modelNum"],
-                shoe_props["productDescription"],
-                shoe_props["releaseDate"],
-                shoe_props["imgFilePath"],
-                shoe_props["collabName"], # this one comes from the original URL DB entry (but is initialized in the kixify section) - not scraped
-                shoe_props["lowestGoatPrice"],
-                shoe_props["lowestFcPrice"],
-                shoe_props["lowestKixPrice"],
-            )
-        # print(SQL_Insert_Query)
-        cursor.execute(SQL_Insert_Query)
-        connection.commit()
+            shoe_props["brandName"],
+            shoe_props["productName"],
+            shoe_props["modelNum"],
+            shoe_props["productDescription"],
+            shoe_props["releaseDate"],
+            shoe_props["imgFilePath"],
+            shoe_props["collabName"], # this one comes from the original URL DB entry (but is initialized in the kixify section) - not scraped
+            shoe_props["lowestGoatPrice"],
+            shoe_props["lowestFcPrice"],
+            shoe_props["lowestKixPrice"]
+        )
+        return SQL_Insert_Query
+    elif (action == "update"):
+        SQL_Update_Query = "UPDATE products SET brandName = \"{}\", productName = \"{}\", modelNum = \"{}\", productDescription = \"{}\", releaseDate = \"{}\", imgFilePath = \"{}\", collabName = \"{}\", lowestGoatPrice = {}, lowestFcPrice = {}, lowestKixPrice = {}"
+        SQL_Update_Query = SQL_Update_Query.format(
+            shoe_props["brandName"],
+            shoe_props["productName"],
+            shoe_props["modelNum"],
+            shoe_props["productDescription"],
+            shoe_props["releaseDate"],
+            shoe_props["imgFilePath"],
+            shoe_props["collabName"],
+            shoe_props["lowestGoatPrice"],
+            shoe_props["lowestFcPrice"],
+            shoe_props["lowestKixPrice"]
+        )
+        SQL_Update_Query += " WHERE imgFilePath = \"{}\"".format(shoe_props["imgFilePath"])
+        return SQL_Update_Query
+
+try:
+    connection = mysql.connector.connect(
+        host = DB_HOST,
+        database = DB_DATABASE,
+        user = DB_USER,
+        password = DB_PASSWORD,
+        auth_plugin = "mysql_native_password" # not always required
+    )
+    SQL_Select_Query = "SELECT * FROM scraper_input"
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(SQL_Select_Query)
+    url_table_entries = cursor.fetchall()
+
+    for row in url_table_entries:
+        shoe_props = scrape_product_properties(row)
+        try:
+            query = get_mysql_query("insert", shoe_props)
+            cursor.execute(query)
+            connection.commit()
+        except Error as e:
+            if (e.errno == 1062): # Duplicate row detected - product already exists
+                query = get_mysql_query("update", shoe_props)
+                cursor.execute(query)
+                connection.commit()
 
     if (connection.is_connected()):
         connection.close()
         cursor.close()
-        print("MySQL connection is closed")
 
 except Error as e:
-    print("Error reading data from MySQL table", e)
+    print("Error updating MySQL table", e)
